6514303889d53f5bee8cdf4014987aabe26489ef
adnankarim2030-dev <adnankarim2030@gmail.com>
2026-08-09 21:03:28 +0500

    Add scraped AFAQ Publishers books

---
+++ assets/js/scraped-afaq.js
@@ -0,0 +1,722 @@
+const SCRAPED_AFAQ = [
+    {
+        "id": "afaq_55008133",
+        "title": "General Knoweldge 3 (English Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-General-Knowledge-3-EM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_63645651",
+        "title": "General Knoweldge 2 (English Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-General-Knowledge-2-EM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_42903318",
+        "title": "General Knoweldge 1 (English Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-General-Knowledge-1-EM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_39458268",
+        "title": "Social Studies 5 (English Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-Social-Studies-5-EM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_54314248",
+        "title": "Social Studies 4 (English Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-Social-Studies-4-EM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_25884506",
+        "title": "Maloomat e Aama : General Knowledge 1 (Urdu Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-General-Knowledge-1-UM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_13272481",
+        "title": "Maloomat e Aama : General Knowledge 2 (Urdu Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-General-Knowledge-2-UM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_87131498",
+        "title": "Maloomat e Aama : General Knowledge 3 (Urdu Medium)",
+        "price": 0.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sun-Series-General-Knowledge-3-UM.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_96342612",
+        "title": "My colouring Book of Salah",
+        "price": 150.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/My-coloring-book-of-salah.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_37483051",
+        "title": "A colouring book Islamic Manners",
+        "price": 150.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/colouring-book-of-islamic-manners-1.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_94131019",
+        "title": "Up and Away with Arabic Numbers",
+        "price": 160.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/up-and-away-with-arabic-numbers-1.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_71604315",
+        "title": "King of Fruits",
+        "price": 225.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/1-Mango-The-king..jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_81826071",
+        "title": "Asma and Bunny",
+        "price": 225.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/2-Asma-and-Bunny.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_93921778",
+        "title": "Bella and Candy",
+        "price": 225.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/1-Bella-Candy.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_69644597",
+        "title": "Taha and Painting",
+        "price": 225.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/2-Talha-and-Painting.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_72671635",
+        "title": "Mother Knows Best",
+        "price": 225.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/4-Mother-Knows-best.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_15935878",
+        "title": "Mutala-e-Islam (KG)",
+        "price": 225.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Islamiyat-KG.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_81245733",
+        "title": "Sail Through with Arabic Letters",
+        "price": 230.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Sail-through-with-arabic-letters.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_63584229",
+        "title": "Sindhi Ibtedai Qaido",
+        "price": 235.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Sindhi-Pre-Nursery.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_50740375",
+        "title": "Looking after Nature",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/4-Looking-after-Nature.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_32443480",
+        "title": "The Crying Forest",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/3-Crying-Forest.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_00688654",
+        "title": "Sister Trouble",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/3-Sister-Trouble.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_00188873",
+        "title": "Aao mil jul kar khelain",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/aao-miljul-kr-khelain-4-5.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_74645270",
+        "title": "Haathi ki Ainak",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/hathi-ki-ainak-5-6.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_71604315",
+        "title": "King of Fruits",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/1-Mango-The-king.-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_69644597",
+        "title": "Taha and Painting",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/2-Talha-and-Painting-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_81826071",
+        "title": "Asma and Bunny",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Asma-and-Bunny.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_32045089",
+        "title": "Ali\u2019s Late Day",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Alis-Late-Day.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_33303147",
+        "title": "Looking after nature",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Looking-after-Nature.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_65537872",
+        "title": "The Proud Peacock",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Proud-Peacock.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_82772601",
+        "title": "B-Khargosh",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/B-Khargosh-3-4-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_72846697",
+        "title": "Animals on farm",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/2-Animals-on-Farm-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_93921778",
+        "title": "Bella and Candy",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/1-Bella-Candy-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_00688654",
+        "title": "Sister Trouble",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/3-Sister-Trouble-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_72671635",
+        "title": "Mother Knows Best",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/4-Mother-Knows-best-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_85442830",
+        "title": "Zakhmi Kabootar",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Zakhmi-Kabootar-3-4-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_72929496",
+        "title": "Khala ka Ghar",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Khala-ka-ghar-4-5-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_29262006",
+        "title": "Makhi aur Chunti",
+        "price": 245.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Bee-and-Ant-5-6-1.jpg",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_22088414",
+        "title": "Life and Sayings of Rasulullah",
+        "price": 260.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Life-and-Sayings-of-RASULULLAH.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_81056048",
+        "title": "English Grammar 1",
+        "price": 280.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/English-Grammar-1.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_84535718",
+        "title": "Art and Drawing (Pre-Nursery)",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Iqbal-Series-Art-Drawing-Pre-Nursery.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_23860300",
+        "title": "Mutala-e-Islam &#8211; 1",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Iqbal-Series-Islamiyat-1.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_59374653",
+        "title": "Urdu Grammar 2",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2022/08/Urdu-Grammar-2.png",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_00012865",
+        "title": "Sindhi Qaido 1",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Sindhi-Nursery.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_66272034",
+        "title": "Sindhi Qaido 2",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Sindhi-KG.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_87435479",
+        "title": "Sindhi Book 3",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Sindhi-3.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_95743760",
+        "title": "Sindhi Book 4",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Sindhi-4-1.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    },
+    {
+        "id": "afaq_93858731",
+        "title": "Sindhi Book 5",
+        "price": 295.0,
+        "img": "https://afaq.edu.pk/wp-content/uploads/2026/05/Iqbal-Series-Sindhi-5.webp",
+        "pub": "AFAQ Publishers",
+        "category": "Books",
+        "type": "Book",
+        "cls": [
+            "General"
+        ],
+        "subj": [
+            "General"
+        ]
+    }
+];
